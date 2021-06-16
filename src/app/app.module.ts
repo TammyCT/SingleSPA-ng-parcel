43e31944d87996b40b18fd578218b0dae596422f
Tammy Huang <tammy.huang@bamboonetworks.com>
2021-06-16 17:43:36 +0800

    support single spa
    Signed-off-by: Tammy Huang <tammy.huang@bamboonetworks.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { SpeakerComponent } from './speaker/speaker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyRouteComponent,
+    SpeakerComponent
   ],
   imports: [
     BrowserModule,
